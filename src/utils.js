5f02a968f7846efb502261d4dd66a54c92ab2782
ariful4082 <ariful4082@gmail.com>
2022-03-29 10:01:42 +0600

    use JS Doc

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const dbPath = path.resolve('data', 'db.json');
+const fs = require("fs");
+const path = require("path");
+const dbPath = path.resolve("data", "db.json");
 
-
-exports.readFile = async()=>{
-    const data = await fs.readFile(dbPath);
-    return JSON.parse(data)
-}
-
-exports.writeFile = async(data)=>{
-    await fs.writeFile(dbPath, JSON.stringify(data))
-}
+/**
+ * Thi is database data write function
+ * @returns {any} data
+ */
+exports.readFile = async () => {
+  const data = await fs.readFile(dbPath);
+  return JSON.parse(data);
+};
+/**
+ * This is database write Function
+ * @param {any} data}
+ */
+exports.writeFile = async (data) => {
+  await fs.writeFile(dbPath, JSON.stringify(data));
+};
