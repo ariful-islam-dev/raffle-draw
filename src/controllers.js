da660cf598b0f0c22ffc804453a254c2554efe98
ariful4082 <ariful4082@gmail.com>
2022-03-30 13:00:21 +0600

    sell ticket collection

--- src/controllers.js
+++ src/controllers.js
@@ -1,15 +1,23 @@
-
-
-const ticketCollection = require('./tickiets');
+const ticketCollection = require("./tickiets");
 
 // ticket selling controllers
 
-exports.sellSingleTicket = (req, res, next)=>{
-    const {username, price}= req.body;
+exports.sellSingleTicket = (req, res, next) => {
+  const { username, price } = req.body;
+  const ticket = ticketCollection.create(username, price);
 
-    const ticket = ticketCollection.create(username, price)
-}
+  res.status(201).json({
+    message: "Ticket Created Successfully",
+    ticket,
+  });
+};
 
-exports.sellBulkTicket = (req, res, next)=>{
+exports.sellBulkTicket = (req, res, next) => {
+    const {username, price, quantity}=req.body;
+    const tickets = ticketCollection.createBulk(username, price, quantity);
 
-}
+    res.status(201).json({
+        message: 'Tickets created Successfully',
+        tickets
+    })
+};
