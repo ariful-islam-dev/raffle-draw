4c0ba8fdc923858f1d6d636597545801656e3a67
ariful4082 <ariful4082@gmail.com>
2022-03-31 07:30:44 +0600

    update controllers

--- src/controllers.js
+++ src/controllers.js
@@ -43,9 +43,36 @@ exports.findById = (req, res, next)=>{
 
 // Find By username
 
-exports.findByUsername = (req, res,_next)=>{
+exports.findByUsername = (req, res )=>{
   const username = req.params.username;
   const tickets = ticketCollection.findTicketByUsername(username);
   res.status(200).json({items: tickets, total: tickets.length})
 
+}
+
+//update Controller by id
+
+exports.updateById = (req, res)=>{
+  const id = req.params.id;
+  const ticket = ticketCollection.findTicketById(id);
+
+  if(!ticket){
+    return res.status(404).json({message: '404 Not Found'})
+  }
+
+  const updateTicket = ticketCollection.updateTicketById(id, req.body)
+  res.status(203).json(updateTicket)
+
+}
+
+exports.updateByUsername=(req,res)=>{
+  const username = req.params.username;
+  const tickets = ticketCollection.findTicketByUsername(username)
+
+  if(!tickets){
+    return res.status(404).json({message: '404 Not Found'})
+  }
+
+  const updateTicket = ticketCollection.updateBulk(username, req.body)
+  res.status(200).json({items: tickets, total: tickets.length})
 }
--- src/routs.js
+++ src/routs.js
@@ -1,24 +1,30 @@
-const { sellSingleTicket, sellBulkTicket, findByUsername, findById, findAll } = require('./controllers');
+const {
+  sellSingleTicket,
+  sellBulkTicket,
+  findByUsername,
+  findById,
+  findAll,
+  updateById,
+  updateByUsername,
+} = require("./controllers");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 // router.get('/t/:id')
 // router.put('/t/:id')
 // router.delete('/t/:id')
 
-router.route('/t/:id').get(findById).put().delete();
+router.route("/t/:id").get(findById).put(updateById).delete();
 
 // router.get('/u/:username')
 // router.put('/u/:username')
 // router.delete('/u/:username')
 
-router.route('/u/:username').get(findByUsername).put().delete();
+router.route("/u/:username").get(findByUsername).put(updateByUsername).delete();
 
+router.post("/bulk", sellBulkTicket);
+router.get("/draw");
 
-router.post('/bulk', sellBulkTicket)
-router.get('/draw')
-
-router.route('/').port(sellSingleTicket).get(findAll)
-
+router.route("/").port(sellSingleTicket).get(findAll);
 
 module.exports = router;
